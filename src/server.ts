be98da1ee98c1e2a2df3a8a0c683b37241f56b3e
HolyBytes <sppquicksmk@gmail.com>
2026-05-24 15:41:58 +0700

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,16 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
-
 import express, { Application, Request, Response } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
-import routes from '@/routes';
+import routes from './routes';
 
 const app: Application = express();
 
-// middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(morgan('tiny'));
 app.use(cors({ origin: '*' }));
-
 app.use(routes);
 
 app.get('/', (req: Request, res: Response) => {
