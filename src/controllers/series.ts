47a60e03b7730cc931a1b6e643edafd7abcebf93
HolyBytes <sppquicksmk@gmail.com>
2026-05-25 01:25:52 +0700

    Refactor axios requests for series endpoints

--- src/controllers/series.ts
+++ src/controllers/series.ts
@@ -1,81 +1,44 @@
 import axios from 'axios';
 import { NextFunction as Next, Request, Response } from 'express';
 import { scrapeSeries, scrapeSeriesDetails } from '../scrapers/series';
-
 type TController = (req: Request, res: Response, next?: Next) => Promise<void>;
-
 export const latestSeries: TController = async (req, res) => {
     try {
         const { page = 0 } = req.query;
-        const axiosRequest = await axios.get(
-            `${process.env.ND_URL}/latest-series${
-                Number(page) > 1 ? `/page/${page}` : ''
-            }`
-        );
+        const axiosRequest = await axios.get(`${process.env.ND_URL}/latest-series${Number(page) > 1 ? `/page/${page}` : ''}`);
         const payload = await scrapeSeries(req, axiosRequest);
         res.status(200).json(payload);
-    } catch (err) {
-        console.error(err);
-        res.status(400).json(null);
-    }
+    } catch (err) { console.error(err); res.status(400).json(null); }
 };
-
 export const popularSeries: TController = async (req, res) => {
     try {
         const { page = 0 } = req.query;
-        const axiosRequest = await axios.get(
-            `${process.env.ND_URL}/populer${
-                Number(page) > 1 ? `/page/${page}` : ''
-            }`
-        );
+        const axiosRequest = await axios.get(`${process.env.ND_URL}/populer${Number(page) > 1 ? `/page/${page}` : ''}`);
         const payload = await scrapeSeries(req, axiosRequest);
         res.status(200).json(payload);
-    } catch (err) {
-        console.error(err);
-        res.status(400).json(null);
-    }
+    } catch (err) { console.error(err); res.status(400).json(null); }
 };
-
 export const recentReleaseSeries: TController = async (req, res) => {
     try {
         const { page = 0 } = req.query;
-        const axiosRequest = await axios.get(
-            `${process.env.ND_URL}/release${
-                Number(page) > 1 ? `/page/${page}` : ''
-            }`
-        );
+        const axiosRequest = await axios.get(`${process.env.ND_URL}/release${Number(page) > 1 ? `/page/${page}` : ''}`);
         const payload = await scrapeSeries(req, axiosRequest);
         res.status(200).json(payload);
-    } catch (err) {
-        console.error(err);
-        res.status(400).json(null);
-    }
+    } catch (err) { console.error(err); res.status(400).json(null); }
 };
-
 export const topRatedSeries: TController = async (req, res) => {
     try {
         const { page = 0 } = req.query;
-        const axiosRequest = await axios.get(
-            `${process.env.ND_URL}/rating${
-                Number(page) > 1 ? `/page/${page}` : ''
-            }`
-        );
+        const axiosRequest = await axios.get(`${process.env.ND_URL}/rating${Number(page) > 1 ? `/page/${page}` : ''}`);
         const payload = await scrapeSeries(req, axiosRequest);
         res.status(200).json(payload);
-    } catch (err) {
-        console.error(err);
-        res.status(400).json(null);
-    }
+    } catch (err) { console.error(err); res.status(400).json(null); }
 };
-
 export const seriesDetails: TController = async (req, res) => {
     try {
         const { id } = req.params;
         const axiosRequest = await axios.get(`${process.env.ND_URL}/${id}`);
         const payload = await scrapeSeriesDetails(req, axiosRequest);
         res.status(200).json(payload);
-    } catch (err) {
-        console.error(err);
-        res.status(400).json(null);
-    }
+    } catch (err) { console.error(err); res.status(400).json(null); }
 };
